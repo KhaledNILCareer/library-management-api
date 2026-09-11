1530e8508b571a2105e947cf805733f14110224c
AhmedHosny1998 <hosny4620@gmail.com>
2026-09-11 15:33:09 +0300

    fix(borrows): revert books module route change - coordinate with books owner

--- src/modules/books/book.routes.js
+++ src/modules/books/book.routes.js
@@ -1,8 +1,5 @@
 import { Router } from "express";
-import { borrowBook } from "../borrows/borrow.controller.js";
 
 const router = Router();
-
-router.post("/:id/borrow", borrowBook);
 
 export default router;
